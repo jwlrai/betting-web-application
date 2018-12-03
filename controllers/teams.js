d313e353ae25aef348348c9040bc97f7e9204eab
harrystacey123 <harrystacey@live.co.uk>
2018-12-03 15:05:08 -0800

    added function which gets the teams

--- controllers/teams.js
+++ controllers/teams.js
@@ -2,13 +2,35 @@ const express = require(`express`);
 const route = express.Router();
 const teams = require(`../modules/teams`);
 
-console.log(`inside`);
+route.get(`/get`, (req, res) => {
+    const teamName = req.body.teamName;
+    const description = req.body.description;
+    const imgLink = req.body.imgLink;
+    teams.getTeams(teamName, description, imgLink, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        };
+    });
+});
 
 route.post(`/create`, (req, res) => {
     const teamName = req.body.teamName;
     const description = req.body.description;
     const imgLink = req.body.imgLink;
     teams.createTeams(teamName, description, imgLink, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(data);
+        };
+    });
+});
+
+route.delete(`/:teamId`, (req, res) => {
+    const matchId = req.body.matchId;
+    teams.deleteTeams(matchId, (req, res) => {
         if (err) {
             console.log(err);
         } else {
--- modules/teams.js
+++ modules/teams.js
@@ -2,11 +2,35 @@ const db = require(`../models`);
 
 module.exports = {
 
+    getTeams: function(teams, description, imgLink, cb) {
+        db.teams.find({
+        },
+        (err, data) => {
+            if (err) {
+                cb(err, null);
+            } else {
+                cb(false, data);
+            };
+        });
+    },
+
     createTeams: function(teams, description, imgLink, cb) {
         db.teams.create({
-            teams: teams,
+            name: teams,
             description: description,
             imgLink: imgLink
+        },
+        (err, data) => {
+            if (err) {
+                cb(err, null);
+            } else {
+                cb(false, data);
+            };
+        });
+    },
+    deleteTeams: function(matchId, cb) {
+        db.teams.findOneAndRemove({
+            matchId: matchId
         },
         (err, data) => {
             if (err) {
