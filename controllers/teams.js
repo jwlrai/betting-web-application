cdb91c3e47df037392e2d7cb828cc0bf48cbcb95
harrystacey123 <harrystacey@live.co.uk>
2018-12-04 11:43:42 -0800

    added image upload functionality

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,6 +1,16 @@
 const express = require(`express`);
 const route = express.Router();
 const teams = require(`../modules/teams`);
+const multer = require(`express`);
+const storage = multer.diskStorage({
+    destination: `./public/images/upload`,
+filename: function(req, file, cb) {
+    let name = file.originalname.split(`.`);
+    cb(null, file.originalname + `-` + Date.now()+"."+name[1]);
+}
+});
+const upload = multer({ storage: storage});
+
 
 route.get(`/get`, (req, res) => {
     if (res.locals.userData !== null) {
@@ -17,11 +27,11 @@ route.get(`/get`, (req, res) => {
 
 });
 
-route.post(`/create`, (req, res) => {
+route.post(`/create`, upload.single(`teamImg`), (req, res) => {
     if (res.locals.userData !== null && res.locals.userData.group == `admin`){
         const teamName = req.body.teamName;
         const description = req.body.description;
-        const imgLink = req.body.imgLink;
+        const imgLink = `/images/upload/` + req.files.filename;
         teams.createTeams(teamName, description, imgLink, (err, data) => {
             if (err) {
                 console.log(err);
