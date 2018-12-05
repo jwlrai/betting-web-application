f676f31bedee35cb162365cfb6526b6934c0d67d
jwlrai <jwolrai@gmail.com>
2018-12-05 15:49:56 -0800

    navigation design modifiend

--- public/js/main.js
+++ public/js/main.js
@@ -4,17 +4,24 @@
         $('nav li a').click(function(e){
             e.preventDefault();
             var page = $(this).attr('href');
+            var ths = $(this);
             if(page !=='account'){
                 $('main > div.container').load(page,function(response, status, xhr){
-                   
+                    ths.parents('ul').children('li').removeClass('nav-sel');
+                   console.log(page);
                     if(page=='/logout'){
                         window.location='/';
+                    }
+                    else if(page=='/page/profile'){
+                        ths.parents('ul').parent('li').addClass('nav-sel');
+                    }else{
+                        ths.parent('li').addClass('nav-sel');
                     }
                 });
             }
            
         });
-        
+
         // sending image to server
         // jq('#formstest').submit(function(e){
         //     e.preventDefault();
